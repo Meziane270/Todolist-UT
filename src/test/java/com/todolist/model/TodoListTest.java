3fb1404c7bea5d1dded53557b7a9547355bde76d
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2021-04-29 17:03:21 +0200

    [ADD] Test for TodoList

--- src/test/java/com/todolist/model/TodoListTest.java
+++ src/test/java/com/todolist/model/TodoListTest.java
@@ -1,22 +1,52 @@
 package com.todolist.model;
 
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TodoListTest extends TestCase {
+import java.time.LocalDate;
 
-    private User user;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TodoListTest {
+
+    private String uniqueItemName;
+    private TodoList emptyTodoList;
+    private TodoList fullTodoList;
+    private TodoList oneItemTodoList;
 
     @Before
     public void setUp() {
+        emptyTodoList = new TodoList();
+        oneItemTodoList = new TodoList();
+        uniqueItemName = "unique";
+        oneItemTodoList.addItem(new Item(uniqueItemName,"aaaa", LocalDate.of(2020,1,1)));
+        fullTodoList = new TodoList();
+        for(int i=0;i<10;i++){
+            fullTodoList.addItem(new Item(String.valueOf(i),"bbbb", LocalDate.of(2020,i+1,1)));
+        }
     }
 
     @Test
     public void numberOfItemIsFine(){
+        Item item = new Item("1","a");
+        emptyTodoList.addItem(item);
+        assertTrue(emptyTodoList.getItems().contains(item));
     }
 
     @Test
     public void numberOfItemIsNotFine(){
+        Item item = new Item("11","a");
+        fullTodoList.addItem(item);
+        assertFalse(fullTodoList.getItems().contains(item));
+        assertTrue(fullTodoList.getItems().size()<=10);
     }
+
+    @Test
+    public void insertAlreadyUsedNameItem(){
+        Item item = new Item(uniqueItemName,"aaa");
+        oneItemTodoList.addItem(item);
+        assertFalse(oneItemTodoList.getItems().contains(item));
+    }
+
 }
