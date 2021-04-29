2b413c08daea8625668c30a331b9872831d32f7d
Damien Da Fonseca <da.fonseca.damien@gmail.com>
2021-04-29 15:39:40 +0200

    [ADD] Test Method to Item and TodoList

--- src/test/java/com/todolist/model/ItemTest.java
+++ src/test/java/com/todolist/model/ItemTest.java
@@ -2,10 +2,22 @@ package com.todolist.model;
 
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ItemTest extends TestCase {
+
+    private User user;
+
     @Before
     public void setUp() {
+        StringBuffer toDoListcontentTooLong = new StringBuffer(1500);
+        for(int i=0;i<1500;i++){
+            toDoListcontentTooLong.append("a");
+        }
+    }
+
+    @Test
+    public void contentLengthTooLong(){
     }
 
 }
--- src/test/java/com/todolist/model/TodoListTest.java
+++ src/test/java/com/todolist/model/TodoListTest.java
@@ -2,10 +2,21 @@ package com.todolist.model;
 
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
 
 public class TodoListTest extends TestCase {
+
+    private User user;
+
     @Before
     public void setUp() {
     }
 
+    @Test
+    public void numberOfItemIsFine(){
+    }
+
+    @Test
+    public void numberOfItemIsNotFine(){
+    }
 }
