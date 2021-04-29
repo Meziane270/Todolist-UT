3f773fd535f454d618ce884c6fc4e47faeccc5ea
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2021-04-29 16:14:20 +0200

    update toDoList.model.java

--- src/main/java/com/todolist/model/TodoList.java
+++ src/main/java/com/todolist/model/TodoList.java
@@ -11,8 +11,27 @@ import java.util.ArrayList;
 public class TodoList {
     @Singular
     private final ArrayList<Item> items = new ArrayList<>();
+    public String email;
+    public ArrayList<Item> itemList;
 
-    public void addItem(Item item) {
+    public TodoList() {
+        this.itemList = new ArrayList<>();
+    }
 
+    public void addItem(Item itemToAdd){
+        if(this.isListFull()){
+            return;
+        }
+        this.itemList.add(itemToAdd);
+        if(this.itemList.size() == 8 ){
+            // sendEmail()
+        }
+    }
+
+    public boolean isListFull(){
+        if ( this.itemList.size() < 10){
+            return false;
+        }
+        return true;
     }
 }
