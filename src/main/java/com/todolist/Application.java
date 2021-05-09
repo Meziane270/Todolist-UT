890477e3df4eea5feed4be5fec00c4324e7c3e94
pooetitu <pooetitu@gmail.com>
2021-05-09 21:59:29 +0200

    Add spring starter removed unused libs

---
+++ src/main/java/com/todolist/Application.java
@@ -0,0 +1,16 @@
+package com.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
