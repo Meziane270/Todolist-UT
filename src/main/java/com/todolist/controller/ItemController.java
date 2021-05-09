a10b5d96c740dc5700f66391f007688a8ea8ade5
pooetitu <pooetitu@gmail.com>
2021-05-09 20:42:51 +0200

    Implement user controller

--- src/main/java/com/todolist/controller/ItemController.java
+++ src/main/java/com/todolist/controller/ItemController.java
@@ -1,9 +1,10 @@
 package com.todolist.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/item")
 public class ItemController {
+
 }
