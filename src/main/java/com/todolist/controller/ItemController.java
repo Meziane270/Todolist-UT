2ab86ffe04267c8b4b82904146249cadf4f08e8c
pooetitu <pooetitu@gmail.com>
2021-05-09 18:51:26 +0200

    Create controllers

---
+++ src/main/java/com/todolist/controller/ItemController.java
@@ -0,0 +1,9 @@
+package com.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/item")
+public class ItemController {
+}
