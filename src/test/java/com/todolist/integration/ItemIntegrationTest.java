160d377847225a1919de5f5c863812383df67972
pooetitu <pooetitu@gmail.com>
2021-06-11 13:47:05 +0200

    Implement create new valid user test

--- src/test/java/com/todolist/integration/ItemIntegrationTest.java
+++ src/test/java/com/todolist/integration/ItemIntegrationTest.java
@@ -1,4 +1,39 @@
 package com.todolist.integration;
 
+import com.todolist.unit.User;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemIntegrationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @BeforeEach
+    public void setup() {
+    }
+
+    @Test
+    public void createNewValidUser() {
+        ResponseEntity<User> responseEntity = restTemplate.postForEntity("/user", new User("test@mail.com", "firstname", "lastname", "password123", LocalDate.now()), User.class);
+        User user = responseEntity.getBody();
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertNotNull(user);
+        assertEquals("firstname", user.getFirstname());
+    }
 }
