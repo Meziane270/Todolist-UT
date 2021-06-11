e1a7dda86bdee1e5e36ff6086848da3e22208a6e
pooetitu <pooetitu@gmail.com>
2021-06-11 20:31:05 +0200

    Implement todolist get by id integration test

--- src/test/java/com/todolist/integration/TodoListIntegrationTest.java
+++ src/test/java/com/todolist/integration/TodoListIntegrationTest.java
@@ -1,4 +1,56 @@
 package com.todolist.integration;
 
+import com.todolist.repository.UserRepository;
+import com.todolist.unit.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class TodoListIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private long todoListId;
+
+    @Before
+    public void setup() {
+        User user = new User("test@mail.com", "firstname", "lastname", "password123", LocalDate.now());
+        user = userRepository.save(user);
+        todoListId = user.getTodoList().getId();
+    }
+
+    @Test
+    public void getByIdTodoListTest() throws Exception {
+        this.mockMvc.perform(get("/todo-list/{id}", todoListId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.items").isArray())
+                .andReturn();
+    }
 }
